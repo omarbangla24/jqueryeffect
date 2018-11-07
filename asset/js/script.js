5077dc647b90ad2c31e704e64fcefb94773deb44
Omar Faruk <39029602+omarbangla24@users.noreply.github.com>
2018-11-07 10:32:23 +0600

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function(){
+        $(".arrow").click(function () {
+                $(".social-box").toggle(500)
+        });
+
+
+    $(".arrow").click(function () {
+        $(".a-l-icon").toggle()
+    });
+
+            // Item Start
+
+    $("h1").click(function(){
+        $("#breakfast").slideToggle();
+        $("#dinner").hide();
+        $("#lunch").hide();
+    });
+
+    $("h2").click(function(){
+        $("#lunch").slideToggle();
+        $("#breakfast").hide();
+        $("#dinner").hide();
+    });
+
+    $("h3").click(function(){
+        $("#dinner").slideToggle();
+        $("#breakfast").hide();
+        $("#lunch").hide();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
